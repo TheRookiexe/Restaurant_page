88a8f0aa57c3023b8970982b1bf1e10ab7ecf094
TheRookiexe <mishrxa36@gmail.com>
2025-11-02 20:07:56 +0530

    Organaized modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./styles.css";
-import { loadPg } from "./load-page";
+import { loadPg } from "./components/load-page";
 
 const home = document.getElementById('hm');
 
 console.log("test");
 loadPg()
+
 // home.addEventListener('click', loadPg);
