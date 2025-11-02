97c945b20306be730bc58c7e5904d1d20af56085
TheRookiexe <mishrxa36@gmail.com>
2025-11-02 21:28:48 +0530

    added the menu page Module

--- src/components/menu-page.js
+++ src/components/menu-page.js
@@ -5,6 +5,42 @@ function menuPg(){
     menuHeader.classList.add('munHdDiv');
     menuHeader.textContent = "Menu";
     contentDiv.appendChild(menuHeader);
+    const list = document.createElement('div');
+    list.classList.add('list');
+    list.innerHTML = `<p class="title">Starters / Appetizers</p>
+Garlic Bread with Cheese
+Crispy French Fries
+Veg/Chicken Spring Rolls
+Tomato Basil Soup
+Paneer Tikka / Chicken Tikka
+Caesar Salad
+Nachos with Salsa
+
+<p class="title">Main Course</p>
+Margherita Pizza
+BBQ Chicken Pizza
+Veggie Delight Burger
+Grilled Chicken Sandwich
+Paneer Butter Masala with Naan
+Chicken Biryani
+Alfredo Pasta / Arrabbiata Pasta
+
+<p class="title">Desserts</p>
+Chocolate Lava Cake
+Gulab Jamun
+Cheesecake
+Ice Cream Sundae
+Brownie with Ice Cream
+Rasmalai
+
+<p class="title">Drinks / Beverages</p>
+Cold Coffee
+Fresh Lime Soda
+Cappuccino
+Soft Drinks
+Fresh Juice
+`;
+contentDiv.appendChild(list);
 
 }
 
